9e98f49f6bf8046cc54abaa7100297d2ba89bd6a
Vann <tvanthey@gmail.com>
2022-06-01 15:41:52 +0700

    finished dashboard activity

--- app/src/main/java/com/example/student/DashboardActivity.java
+++ app/src/main/java/com/example/student/DashboardActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.example.student.dashboard.Adapter;
 import com.example.student.dashboard.CategoryFragment;
@@ -19,6 +20,15 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        //name student on dashboard
+        TextView student_name_dashboard = findViewById(R.id.student_name_dashboard);
+        //student rank on dashboard
+        TextView student_rank_dashboard = findViewById(R.id.student_rank_dashboard);
+        TextView student_credit_dashboar = findViewById(R.id.student_credit_dashboard);
+
+        student_rank_dashboard.setText("#2nd Rank");
+        student_credit_dashboar.setText("500");
+        student_name_dashboard.setText("Thorng Vanthey");
 //        call fragment category
         getSupportFragmentManager().beginTransaction().replace(R.id.Frame_category,new CategoryFragment()).commit();
 //        call fragment menu
--- app/src/main/java/com/example/student/LoginActivity.java
+++ app/src/main/java/com/example/student/LoginActivity.java
@@ -2,7 +2,12 @@ package com.example.student;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //underline forgot password text
+        TextView forgot_password_login = findViewById(R.id.forgot_password_login);
+        forgot_password_login.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG);
+
+        Button student_btn_login = findViewById(R.id.student_btn_login);
+
+        student_btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
