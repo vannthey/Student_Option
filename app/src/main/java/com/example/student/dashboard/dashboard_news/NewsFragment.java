08b0b7d79c1ff9146bcb80abf75f5d374d190f2b
Vann <tvanthey@gmail.com>
2022-06-18 10:24:48 +0700

    clean up project

--- app/src/main/java/com/example/student/dashboard/dashboard_news/NewsFragment.java
+++ app/src/main/java/com/example/student/dashboard/dashboard_news/NewsFragment.java
@@ -18,21 +18,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NewsFragment extends Fragment {
-
-//    RecyclerView recyclerView;
-//    List<String> itemList;
-//    Adapter_news adapter;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_newsfragment, container, false);
-//        recyclerView=view.findViewById(R.id.recycler_view_news);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-//        itemList.add(new Homework("Data Structure","15,June,2022","6:00 to 8:00"));
-//        adapter = new Adapter_news(getContext(),itemList);
-//        recyclerView.setAdapter(adapter);
         return view;
     }
 
